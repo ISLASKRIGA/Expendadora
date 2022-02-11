08cfe56975fb5b936dd8fb08af667a5af4bfc76f
chuch <chuch@LAPTOP-RG44R8BT>
2022-02-11 12:52:18 -0600

    Agregar archivos de proyecto.

---
+++ Expendedora1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace Expendadora
+{
+    internal class Expendedora1
+    {
+        #region Atributos  
+        string marca;
+        byte temperatura;
+        float precio;
+        ushort cantProductos;
+        #endregion
+
+        public Expendedora1() 
+        {
+            marca = "ADM";
+            temperatura = 18;
+            Saludar();
+            LimpiarPantalla();
+            Console.WriteLine("Marca:{0}", marca);
+            LimpiarPantalla();
+            Console.WriteLine("Temperatura:{0}",temperatura);
+            LimpiarPantalla();
+            string codigo= MostrarCodigoProducto();
+            LimpiarPantalla();
+            MostrarPrecio(codigo);
+
+
+        }
+
+        #region Métodos
+
+        void Saludar()
+
+        {
+            Console.WriteLine("**Bienvenido a tu expendedora**");
+            
+        }
+        void LimpiarPantalla()
+        {
+            Thread.Sleep(2000);//el parametro está en milisegundos
+            Console.Clear();
+        }
+
+        string MostrarCodigoProducto()
+        {
+            Console.WriteLine(" 1A) Crujitos \n 2B) Totis ");
+            Console.WriteLine("Ingresa el codigo del producto a elegir: ");
+            string codigo = Console.ReadLine();//Variable local
+            return codigo;
+        }
+
+        void MostrarPrecio(string codigo)
+        {
+            switch(codigo)
+            {
+                case "1A":
+                    precio = 13;// Todo atributo tiene un valor por default
+                                //Para este caso en 0.0
+                    break;
+                case "2B":
+                    precio = 10;
+                    break;
+            }
+            Console.WriteLine("El precio es:{0}", precio);
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Expendadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objeto de la Clase
+            Expendedora1 miExpendedora = new Expendedora1();
+            Console.ReadLine();
+
+        }
+    }
+}
